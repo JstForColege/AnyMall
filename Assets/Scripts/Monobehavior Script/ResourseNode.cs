3fb4fc2627e3b5e3a0ca93fa9088fc4a5fdf5668
GriszinStav <ewedwedw@gmail.com>
2026-07-04 16:53:35 +0300

    добавлен_метод_Grow_в_ResourceNode

--- Assets/Scripts/Monobehavior Script/ResourseNode.cs
+++ Assets/Scripts/Monobehavior Script/ResourseNode.cs
@@ -1,24 +1,55 @@
-﻿using System;
+﻿using Mono.Cecil;
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class ResourceNode
+public class ResourceNode : MonoBehaviour
 {
-	/*private int _amount;
+	private int _amount;
 	private int _timer;
-	private Resourse _food; //Resource - класс, который создаст Юсуф
+	private int _currentAmount = 0;
+	private ResourceFood _food;
 
 	public int Amount
 	{
 		get => _amount; 
 		set => _amount = value;
 	}
+	public int CurrentAmount
+	{
+		get => _currentAmount;
+		set => _currentAmount = value;
+	}
 	public int Timer
 	{
 		get => _timer;
 		set => _timer = value;
 	}
-	public Resource Food
+	public ResourceFood Food
 	{
 		get => _food;
 		set => _food = value;
-	}*/
+	}
+
+	public void Grow()
+	{
+		if (CurrentAmount <= Amount)
+		{
+			StartCoroutine(GrowCoroutine());
+		}
+	}
+
+	public IEnumerator GrowCoroutine()
+	{
+		++CurrentAmount;
+		yield return new WaitForSeconds(Timer);
+	}
+}
+
+
+public class ResourceFood //Заглушка покаа Юсуф не сделает класс
+{
+	public int id;
 }
