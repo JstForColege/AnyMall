88b225ab867d1cabc337fd015a5f9dc0177f1571
Jirill001 <kirillsimohin@gmail.com>
2026-07-05 18:10:25 +0300

    Обновлён UISystem

--- Assets/Scripts/UIScripts/UISystem.cs
+++ Assets/Scripts/UIScripts/UISystem.cs
@@ -1,8 +1,42 @@
 using UnityEngine;
 
+//                              ГАЙД ПО РАБОТЕ С ОКНАМИ (Код ниже)
+// 
+// 1. ВАЖНОЕ ПРЕДУПРЕЖДЕНИЕ
+//     Из-за малого кол-ва времени, скрипт получился одноразовый 
+//     То есть один код работает только с одни окном
+//     Чтобы добавить ещё, вы должны добавить этот код на объект UIManager на сцене и
+//     добавить ссылку на своё окно(предварительно сделав его SetActive(false))
+//
+// 2. ОТКРЫТИЕ ОКНА
+//     Чтобы заставить ваше окно открыться, вы должны нацепить объект UIManager в поле OnClick() у нужной кнопки
+//     После чего выбрать в качестве функции UISystem и выбрать метод OpenUI()
+//     
+// 3. ЗАКРЫТИЕ ОКНА
+//     Чтобы заставить ваше окно закрыться, вы должны нацепить объект UIManager в поле OnClick() у нужной кнопки
+//     После чего выбрать в качестве функции UISystem и выбрать метод CloseUI()
+//     
+
+
+
 public class UISystem : MonoBehaviour
 {
+
+    private static UISystem _instance;
     public GameObject UI;
+    public static UISystem Instance => _instance;
+    private void Awake()
+    {
+
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        _instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
     public void OpenUI()
     {
         UI.SetActive(true);
