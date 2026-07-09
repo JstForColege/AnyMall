b0e0d20d83eeeb76af0e688fedd5e05ab13b9044
GriszinStav <ewedwedw@gmail.com>
2026-07-09 21:48:08 +0300

    префабы_полок_готовы

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -106,6 +106,7 @@ public class PlayerController : MonoBehaviour
             if (inventory.IsEmpty) return;
             Debug.Log("Вижу полку");
             ItemData item = inventory.Peek();
+            Debug.Log(item.Type + "aaaaaaaaaaaaaaaaaaaaaaa");
             if (shelf.AddItem(item))
             {
                 inventory.Pop();
